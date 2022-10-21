d0867bffbc6a78eb5baa84b59cd1eb4ef9a10fca
Arjun Junna <arjun9852@gmail.com>
2022-10-21 17:36:20 +0530

    feat : add filter component

---
+++ src/components/Filter/filter.jsx
@@ -0,0 +1,33 @@
+import './filter.css'
+import {useDataContext} from '../../context/dataContext';
+
+const Filter = () => {
+
+    const {data:{categoryData,selectedCategory},dataDispatch}=useDataContext();
+
+  return (
+     <div class="main__container-filters">
+       {categoryData.map((category)=>{
+        return (
+          <button
+            className={`button btn-chip btn-outline  ${
+              selectedCategory === category.categoryName ? `active` : ``
+            }`}
+            id={category._id}
+            onClick={e =>
+              dataDispatch({
+                type: 'SORT_BY_CATEGORY',
+                payload: category.categoryName,
+              })
+            }
+          >
+            {category.categoryName}
+          </button>
+        );
+}    
+       )}
+     </div>
+  )
+}
+
+export default Filter
