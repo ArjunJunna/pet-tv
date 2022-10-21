0d448d4b4ee3865a27b8887ab27c096d2b30de4a
Arjun Junna <arjun9852@gmail.com>
2022-10-21 21:40:38 +0530

    feat : add explore page

--- src/pages/Explore/explore.jsx
+++ src/pages/Explore/explore.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import '../../utilities/css/util.css'
+import Filter from '../../components/Filter/filter'
+import VideoCard from '../../components/VideoCard/videocard'
+import { useDataContext } from '../../context/dataContext'
 
 const Explore=()=>{
+  const {data: { videoData, selectedCategory }} = useDataContext();
+
+  const filteredData = () => {
+    if (selectedCategory === 'All') {
+      return videoData;
+    } else {
+      return videoData.filter(video => selectedCategory === video.categoryName);
+    }
+  };
+
   return (
-    <h1>Explore</h1>
-  )
+    <>
+      <Filter />
+      <div className="main__container-content">
+        {filteredData().map(video => (
+          <VideoCard video={video} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Explore
