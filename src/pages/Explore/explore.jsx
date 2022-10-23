fa1f985b8d6839f03d27d49ea9360c688a7b8a20
Arjun Junna <arjun9852@gmail.com>
2022-10-23 14:15:12 +0530

    feat : add loader

--- src/pages/Explore/explore.jsx
+++ src/pages/Explore/explore.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import '../../utilities/css/util.css'
-import Filter from '../../components/Filter/filter'
-import VideoCard from '../../components/VideoCard/videocard'
-import { useDataContext } from '../../context/dataContext'
+import React from 'react';
+import '../../utilities/css/util.css';
+import Filter from '../../components/Filter/filter';
+import VideoCard from '../../components/VideoCard/videocard';
+import { useDataContext } from '../../context/dataContext';
 import {
   filteredCategoryData,
   filterBySearch,
 } from '../../utilities/js/filterFunctions';
+import Loader from '../../components/Loader/loader';
 
-const Explore=()=>{
-  const {data: { videoData, selectedCategory,searchFor }} = useDataContext();
+const Explore = () => {
+  const {
+    data: { videoData, selectedCategory, searchFor, isLoading },
+  } = useDataContext();
   const filteredData = filterBySearch(
     filteredCategoryData(videoData, selectedCategory),
     searchFor
@@ -17,14 +20,20 @@ const Explore=()=>{
 
   return (
     <>
-      <Filter />
-      <div className="main__container-content">
-        {filteredData.map(video => (
-          <VideoCard video={video} key={video._id} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Filter />
+          <div className="main__container-content">
+            {filteredData.map(video => (
+              <VideoCard video={video} key={video._id} />
+            ))}
+          </div>
+        </>
+      )}
     </>
   );
-}
+};
 
-export default Explore
+export default Explore;
