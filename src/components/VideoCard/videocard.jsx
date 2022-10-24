a4e69b56229ff6b1f782cbc520b670de409fcb71
Arjun Junna <arjun9852@gmail.com>
2022-10-25 03:57:27 +0530

    feat : add video card dropdown

--- src/components/VideoCard/videocard.jsx
+++ src/components/VideoCard/videocard.jsx
@@ -1,38 +1,67 @@
-import './videocard.css'
+import './videocard.css';
 import { getTrimmedViewCount } from '../../utilities/js/getTrimmedViewCount';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useAuth } from '../../context/authContext';
 
-const VideoCard = ({video,cardDirection}) => {
+const VideoCard = ({ video, cardDirection }) => {
+  const navigate = useNavigate();
+
+  const {
+    auth: { isAuthorized },
+  } = useAuth();
+  const [showActionCard, setShowActionCard] = useState(false);
+
   return (
-   
-      <div className="card__container vertical">
-        <div className="card-header">
-          <img
-            src={video.thumbnailUrl}
-            alt="productimage"
-            className="card-image"
-          />
-          <span className="card-badge">{video.videoLength}</span>
+    <div className="card__container vertical">
+      <div className="card-header">
+        <img
+          src={video.thumbnailUrl}
+          alt="productimage"
+          className="card-image"
+        />
+        <span className="card-badge">{video.videoLength}</span>
+      </div>
+      <div className="card-content">
+        <img
+          src={video.channelImg}
+          alt="person"
+          className="avatar-small circle"
+        />
+        <div className="card-details">
+          <h2 className="detail-heading">{video.title}</h2>
+          <p className="detail-subheading">{video.channelName}</p>
+          <p className="detail-subheading">
+            {getTrimmedViewCount(video.views)} views · {video.releaseDate}
+          </p>
         </div>
-        <div className="card-content">
-          <img
-            src={video.channelImg}
-            alt="person"
-            className="avatar-small circle"
-          />
-          <div className="card-details">
-            <h2 className="detail-heading">{video.title}</h2>
-            <p className="detail-subheading">{video.channelName}</p>
-            <p className="detail-subheading">{getTrimmedViewCount(video.views)} views · {video.releaseDate}</p>
-          </div>
-          <div className="card-footer">
-            <div className="card-icons">
-              <i className="bi bi-three-dots-vertical dots"></i>
-            </div>
+        <div className="card-footer">
+          <div className="card-icons">
+            <i
+              className="bi bi-three-dots-vertical dots"
+              onClick={() => {
+                isAuthorized
+                  ? setShowActionCard(prev => !prev)
+                  : navigate('/login');
+              }}
+            ></i>
+            {showActionCard && (
+              <div className={`${showActionCard} action--card`}>
+                <div className="action--option">
+                  <i className="bi bi-clock-fill"></i>
+                  Watch Later
+                </div>
+                <div className="action--option">
+                  <i className="bi bi-folder-fill"></i>Playlist
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    
+    </div>
   );
 };
 
 export default VideoCard;
+
