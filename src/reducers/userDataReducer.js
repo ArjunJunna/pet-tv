0a177b455eb1da7872ed4e02d56c9fc8266a62ac
Arjun Junna <arjun9852@gmail.com>
2022-10-30 23:26:53 +0530

    feat : add like and watch later states

--- src/reducers/userDataReducer.js
+++ src/reducers/userDataReducer.js
@@ -3,6 +3,12 @@ const userDataReducer = (state, action) => {
     case 'HISTORY_DATA':
       return { ...state, historyData: action.payload };
 
+    case 'LIKED_DATA': 
+      return{...state,likedData:action.payload};
+
+    case 'WATCHLATER_DATA':
+      return{...state, watchlaterData: action.payload};
+
     default:
       return { ...state };
   }
