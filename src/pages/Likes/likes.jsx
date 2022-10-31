6fa3f90f9855e88af5eaea3a8e85ff202369071e
Arjun Junna <arjun9852@gmail.com>
2022-10-31 22:31:12 +0530

    feat : add like and watch later pages

--- src/pages/Likes/likes.jsx
+++ src/pages/Likes/likes.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useUserData } from '../../context/userDataContext'
+import DynamicVideoCard from '../../components/VideoCard/dynamicvideocard'
+import '../../utilities/css/util.css'
 
 const Likes=()=>{
+
+  const {userData:{likedData}}=useUserData();
+
   return (
-    <h1>Likes</h1>
-  )
+    <>
+      {likedData.length ? (
+        <>
+          <div className="info">
+            <p>Watch History</p>
+          </div>
+
+          <div className="main__container-content">
+            <>
+              {likedData.map(video => (
+                <DynamicVideoCard video={video} />
+              ))}
+            </>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="fall-back-text">No videos in your history...</p>
+        </>
+      )}
+    </>
+  );
 }
 
 export default Likes
--- src/pages/WatchLater/watchlater.jsx
+++ src/pages/WatchLater/watchlater.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useUserData } from '../../context/userDataContext';
+import DynamicVideoCard from '../../components/VideoCard/dynamicvideocard';
+import '../../utilities/css/util.css';
+
 
 const WatchLater = () => {
+
+  const {userData:{watchlaterData}}=useUserData();
+
   return (
-    <h1>WatchLater</h1>
-  )
+    <>
+      {watchlaterData.length ? (
+        <>
+          <div className="info">
+            <p>Watch History</p>
+          </div>
+
+          <div className="main__container-content">
+            <>
+              {watchlaterData.map(video => (
+                <DynamicVideoCard video={video} />
+              ))}
+            </>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="fall-back-text">No videos in your history...</p>
+        </>
+      )}
+    </>
+  );
 }
 
 export default WatchLater
