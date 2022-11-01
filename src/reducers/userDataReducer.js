fdfd41890aa9fe4ed72859ae2e154dedd132f38d
Arjun Junna <arjun9852@gmail.com>
2022-11-02 01:37:48 +0530

    feat : playlist context and reducer data

--- src/reducers/userDataReducer.js
+++ src/reducers/userDataReducer.js
@@ -3,11 +3,23 @@ const userDataReducer = (state, action) => {
     case 'HISTORY_DATA':
       return { ...state, historyData: action.payload };
 
-    case 'LIKED_DATA': 
-      return{...state,likedData:action.payload};
+    case 'LIKED_DATA':
+      return { ...state, likedData: action.payload };
 
     case 'WATCHLATER_DATA':
-      return{...state, watchlaterData: action.payload};
+      return { ...state, watchlaterData: action.payload };
+
+    case 'PLAYLIST_DATA':
+      if (action.payload.playlists) {
+        return { ...state, playlistData: action.payload.playlists };
+      } else {
+        const newPlaylist = state.playlistData.map(playlist =>
+          playlist._id === action.payload.playlist._id
+            ? action.payload.playlist
+            : playlist
+        );
+        return { ...state, playlistData: newPlaylist };
+      }
 
     default:
       return { ...state };
